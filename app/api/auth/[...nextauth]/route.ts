47d5f3d22e2132a33b4094debacd7af63c52c7fd
/\n!KeT KuMaR <ankitrazz001@gmail.com>
2026-01-05 23:27:20 +0530

    "next auth signin"

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,40 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Login with username & password",
+      credentials: {
+        username: {
+          label: "Email",
+          type: "text",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+        },
+      },
+
+      async authorize(credentials) {
+        const username = credentials?.username as string;
+        const password = credentials?.password as string;
+        console.log("Username:", username);
+        console.log(" Password:", password);
+        const user ={
+          name:"ankit",
+          id:"1",
+          username:"ankitrazz001@gmil.com"
+        }
+        if(user){
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+
+});
+
+export { handler as GET, handler as POST };
