0316eef7bd807e85900fde0d12350346a8165c03
/\n!KeT KuMaR <ankitrazz001@gmail.com>
2026-01-11 17:00:03 +0530

    "server side insted of usesession hook"

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -34,6 +34,7 @@ const handler = NextAuth({
       },
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET
 
 });
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,44 @@
-"use client"
+// "use client"
+import { getServerSession } from "next-auth";
 import Link from "next/link";
 import { SessionProvider,signIn,signOut,useSession } from "next-auth/react";
+import { get } from "http";
 //usesession hook is the easyest way to check user login or not
-export default function Home(){
-  return <SessionProvider>
-    <RealHome></RealHome>
-  </SessionProvider>
-}
+// export default function Home(){
+//   return <SessionProvider>
+//     <RealHome></RealHome>
+//   </SessionProvider>
+// }
 
-export  function RealHome() {
-  const session = useSession();
+export default async function RealHome() {
+  // const session = useSession();
+  const session = await getServerSession();
   return (
-    <div className="min-h-screen bg-black text-white flex flex-col items-center">
-      <div className="mt-16 bg-white text-black p-10 rounded-xl shadow-lg w-[90%] max-w-xl">
-        {session.status === "authenticated" && <button onClick={()=>signOut()}>Logout</button>}
-        {session.status =="unauthenticated" && <button onClick={()=> signIn()}>signIn</button>}
-        <h1 className="text-3xl font-semibold text-center">Welcome to Todo App</h1>
+    // <div className="min-h-screen bg-black text-white flex flex-col items-center">
+    //   <div className="mt-16 bg-white text-black p-10 rounded-xl shadow-lg w-[90%] max-w-xl">
+    //     {session.status === "authenticated" && <button onClick={()=>signOut()}>Logout</button>}
+    //     {session.status =="unauthenticated" && <button onClick={()=> signIn()}>signIn</button>}
+    //     <h1 className="text-3xl font-semibold text-center">Welcome to Todo App</h1>
 
-        <div className="flex gap-4 justify-center mt-8">
-          <Link 
-            href="/auth//signin" 
-            className="px-4 py-2 bg-gray-200 rounded-lg hover:bg-gray-300 transition"
-          >
-            Sign in to Todo
-          </Link>
+    //     <div className="flex gap-4 justify-center mt-8">
+    //       <Link 
+    //         href="/auth//signin" 
+    //         className="px-4 py-2 bg-gray-200 rounded-lg hover:bg-gray-300 transition"
+    //       >
+    //         Sign in to Todo
+    //       </Link>
 
-          <Link 
-            href="/auth//signup" 
-            className="px-4 py-2 bg-black text-white rounded-lg hover:bg-gray-900 transition"
-          >
-            Sign up to Todo
-          </Link>
-        </div>
-      </div>
+    //       <Link 
+    //         href="/auth//signup" 
+    //         className="px-4 py-2 bg-black text-white rounded-lg hover:bg-gray-900 transition"
+    //       >
+    //         Sign up to Todo
+    //       </Link>
+    //     </div>
+    //   </div>
+    // </div>
+    <div>
+      {JSON.stringify(session)}
     </div>
   );
 }
